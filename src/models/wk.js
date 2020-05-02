a6d27a4059677a4bb6649ed5d7ff0a754b32151b
Oguz Gelal <o.gelal77@gmail.com>
2020-05-02 03:35:24 +0300

    implement logout

--- src/models/wk.js
+++ src/models/wk.js
@@ -11,6 +11,7 @@ class WK {
   // use get user request as a way to validate api key
   // bonus: save the id, username and start date
   login(apiKey) {
+    // TODO: should we move the loading logic to resource ? (yes)
     return resource.get(rs.WK_API_KEY)(() => {
       return new Promise((resolve, reject) => {
         this.req.get('user', { apiKey, loadingKey: 'login' })
@@ -24,14 +25,11 @@ class WK {
     });
   }
 
-  // TODO
   logout() {
-    Object
-      .values(rs)
-      .filter(res => !res.persistOnLogout)
-      .forEach(res => {
-        resource.clearCache(res)
-      })
+    return resource.clearResources((name, _) => {
+      const resource = rs[name];
+      return !resource.persistOnLogout;
+    })
   }
 
   loadReviews() {
