87e6ab289303024de0c153c72b3ec97e9979f81b
Oguz Gelal <o.gelal77@gmail.com>
2020-05-14 02:06:25 +0300

    #27 remove css filter for web on radicals

--- src/components/Image/Radical.js
+++ src/components/Image/Radical.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Image, StyleSheet } from 'react-native';
-import device from 'src/utils/device';
+import { Image } from 'react-native';
 
 const Radical = props => {
   return (
     <Image
       source={{ uri: props.src }}
-      style={[
-        props.style,
-        styles.image,
-      ]}
+      style={props.style}
     />
   )
 };
@@ -19,11 +15,5 @@ Radical.propTypes = {
   src: PropTypes.string,
   style: PropTypes.object,
 };
-
-const styles = StyleSheet.create({
-  image: device({
-    web: { filter: 'invert(45%) sepia(100%) saturate(1274%) hue-rotate(177deg) brightness(101%) contrast(94%)' }
-  })
-})
 
 export default Radical;
