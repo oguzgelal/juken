316268e49d3b939205c5704f322b46bee069cb15
Oguz Gelal <o.gelal77@gmail.com>
2020-11-23 23:02:16 -0500

    remove firebase. change dotenv to env.json

--- src/Main.js
+++ src/Main.js
@@ -4,33 +4,13 @@ import _ from 'lodash';
 import Review from 'src/screens/Review/Review';
 import Login from 'src/screens/Login/Login';
 import Message from 'src/screens/Message/Message';
-import initEvents from 'src/features/events/initEvents';
-import logEvent from 'src/features/events/logEvent';
-import setUserAnalytics from 'src/features/events/setUserAnalytics';
 
 export default () => {
 
   const rehydrated = useStoreRehydrated();
-  const user = useStoreState(state => state.session.user);
   const token = useStoreState(state => state.session.token);
 
   const [ demo, setDemo ] = useState(false);
-
-  useEffect(() => {
-    initEvents();
-
-    // identify and log
-    // user if logged in
-    if (user) {
-      setUserAnalytics(user, () => {
-        logEvent('juken_Load', {
-          user: _.get(user, 'data.username')
-        })
-      });
-    } else {
-      logEvent('juken_Load')
-    }
-  }, [])
 
   // store being rehydrated
   if (!rehydrated) {
