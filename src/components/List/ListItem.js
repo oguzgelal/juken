5a17a7c86be7bed0da4ac89e8362aa3b8f33696e
Oguz Gelal <o.gelal77@gmail.com>
2020-12-17 13:13:10 -0500

    fix list crash issue

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, View, Text } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { StyleSheet, View, Text, Switch } from 'react-native';
+import { ListItem, Icon } from 'react-native-elements';
 import device from 'src/utils/device';
 import useColorScheme from 'src/hooks/useColorScheme';;
 import theme from 'src/common/theme';
@@ -9,17 +9,17 @@ import theme from 'src/common/theme';
 const ListItemComponent = props => {
   const colorScheme = useColorScheme();
   return (
-    <ListItem
-      {...props}
-      leftIcon={!props.leftIcon ? null :
-        <View style={[styles.icon, colorScheme === 'light' ? null : styles.icon_dark]}>{props.leftIcon}</View>}
-      rightIcon={!props.rightIcon ? null :
-        <View style={[styles.icon, colorScheme === 'light' ? null : styles.icon_dark]}>{props.rightIcon}</View>}
-      titleStyle={colorScheme === 'light' ? styles.title : styles.title_dark}
-      subtitleStyle={[styles.subtitle, colorScheme === 'light' ? null : styles.subtitle_dark]}
-      containerStyle={colorScheme === 'light' ? null : styles.container_dark}
-      rightContentContainerStyle={styles.rightContent}
-    />
+    <ListItem {...props}>
+      {props.leftIcon && <View style={[styles.icon, colorScheme === 'light' ? null : styles.icon_dark]}>{props.leftIcon}</View>}
+      <ListItem.Content style={colorScheme === 'light' ? null : styles.container_dark}>
+        <ListItem.Title style={colorScheme === 'light' ? styles.title : styles.title_dark}>{props.title}</ListItem.Title>
+        {props.subtitle && (
+          <Text style={[styles.subtitle, colorScheme === 'light' ? null : styles.subtitle_dark]}>{props.subtitle}</Text>
+        )}
+      </ListItem.Content>
+      {props.rightIcon && <View style={[styles.icon, colorScheme === 'light' ? null : styles.icon_dark]}>{props.rightIcon}</View>}
+      {props.switch && <Switch {...props.switch} />}
+    </ListItem>
   )
 };
 
